72b4a680636258ec02db943aac37b42d5af57882
Nilesh <nileshjaat341@gmail.com>
2020-07-03 12:14:42 +0530

    added default gradient copy

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+const DEFAULT_GRADIENT = linear-gradient(to right, rgb(255, 0, 0), rgb(255, 255, 0));
+
 function setGradient() {
   body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
 
   css.textContent = body.style.background + ";";
-}
+};
 
 function copy(text) {
   var textarea = document.createElement("textarea");
-  textarea.value = text;
+  textarea.value = text || DEFAULT_GRADIENT;
   document.body.appendChild(textarea);
   textarea.select();
   document.execCommand("copy");
